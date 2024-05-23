80f01b17dadcc4f6fddf28d60a1f66ade91a798d
Chris Najman <christopher.najman@gmail.com>
2024-05-23 13:22:16 +0100

    Overwrote Sidebar.jsx with new code

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,33 +1,51 @@
-import React from "react"
+import PropTypes from "prop-types"
+import { FaPlus, FaTrashAlt } from "react-icons/fa"
+function Sidebar(props) {
+  const noteElements = props.notes.map((note) => (
+    <li
+      key={note.id}
+      className={`title ${
+        note.id === props.currentNote.id ? "selected-note" : ""
+      }`}
+      onClick={() => props.setCurrentNoteId(note.id)}
+    >
+      {/* Populate note title with only first line of markdown file */}
+      <h3 className="text-snippet">{note.body.split("\n", 1)[0]}</h3>
+      <button
+        className="btn-delete-note"
+        title="Delete note"
+        onClick={(e) => props.deleteNote(e, note.id)}
+      >
+        <span className="visually-hidden">Delete note</span>
+        <FaTrashAlt aria-hidden="true" />
+      </button>
+    </li>
+  ))
 
-export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
-            <div
-                
-                className={`title ${
-                    note.id === props.currentNote.id ? "selected-note" : ""
-                }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
-                <button 
-                    className="delete-btn"
-                    onClick={(event) => props.deleteNote(event, note.id)}
-                >
-                    <i className="gg-trash trash-icon"></i>
-                </button>
-            </div>
-        </div>
-    ))
-
-    return (
-        <section className="pane sidebar">
-            <div className="sidebar--header">
-                <h3>Notes</h3>
-                <button className="new-note" onClick={props.newNote}>+</button>
-            </div>
-            {noteElements}
-        </section>
-    )
+  return (
+    <section className="pane sidebar">
+      <div className="sidebar--header">
+        <h2>Notes</h2>
+        <button
+          className="new-note"
+          onClick={props.newNote}
+        >
+          <FaPlus aria-hidden="true" />
+          <span className="visually-hidden">Add a new note</span>
+        </button>
+      </div>
+      <ul className="sidebar--items">{noteElements}</ul>
+    </section>
+  )
 }
+
+Sidebar.propTypes = {
+  notes: PropTypes.array,
+  newNote: PropTypes.func,
+  currentNote: PropTypes.object,
+  updateNote: PropTypes.func,
+  setCurrentNoteId: PropTypes.func,
+  deleteNote: PropTypes.func,
+}
+
+export default Sidebar
